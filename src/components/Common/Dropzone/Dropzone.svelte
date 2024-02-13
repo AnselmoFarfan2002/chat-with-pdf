89d677d3b8e4fc63ffdc1745aff4b411ce46df0e
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2024-02-12 23:17:50 -0500

    exploring svelte

---
+++ src/components/Common/Dropzone/Dropzone.svelte
@@ -0,0 +1,18 @@
+<script>
+  import DropzoneButton from "./DropzoneButton.svelte";
+
+  function handleClick() {
+    console.log("asasfd");
+  }
+</script>
+
+<DropzoneButton on:click={handleClick} />
+
+<!-- 
+<input
+  bind:this={refInputFile}
+  type="file"
+  class="hidden"
+  on:change={handleFilesSelect}
+  accept="application/pdf"
+/> -->
---
+++ src/components/Upload.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Dropzone from "./Common/Dropzone/Dropzone.svelte";
+
+  let files = {
+    accepted: [],
+    rejected: [],
+  };
+
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+    files.rejected = [...files.rejected, ...fileRejections];
+  }
+</script>
+
+<Dropzone />
+
+<ol>
+  {#each files.accepted as item}
+    <li>{item.name}</li>
+  {/each}
+</ol>
